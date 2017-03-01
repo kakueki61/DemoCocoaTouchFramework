21cc428486740b79e4d7fa0ffe0ca247ac501310
kakueki61 <t.kodama61@gmail.com>
2017-03-01 16:19:24 +0900

    Initial Commit

---
+++ DemoCocoaTouchFramework/DemoCocoaTouchFramework.h
@@ -0,0 +1,19 @@
+//
+//  DemoCocoaTouchFramework.h
+//  DemoCocoaTouchFramework
+//
+//  Created by 兒玉拓也 on 3/1/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DemoCocoaTouchFramework.
+FOUNDATION_EXPORT double DemoCocoaTouchFrameworkVersionNumber;
+
+//! Project version string for DemoCocoaTouchFramework.
+FOUNDATION_EXPORT const unsigned char DemoCocoaTouchFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DemoCocoaTouchFramework/PublicHeader.h>
+
+
